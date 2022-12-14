0b3855a1c74901155f776715af0704f81aa4c6e4
KhalilFarouqi <k.farouqi@itroadgroup.com>
2022-12-14 15:04:23 +0100

    swagger name and description commit

--- Car_Rental/src/main/java/com/app/CarRentalApplication.java
+++ Car_Rental/src/main/java/com/app/CarRentalApplication.java
@@ -1,9 +1,12 @@
 package com.app;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@OpenAPIDefinition(info = @Info(title = "CAR RENTAL API", version = "2.0", description = "CAR RENTAL Information"))
 public class CarRentalApplication {
 
 	public static void main(String[] args) {

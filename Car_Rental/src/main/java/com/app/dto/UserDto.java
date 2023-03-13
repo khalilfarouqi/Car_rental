c1301cdddeaacaa2f271b898adcc9be1d75f1278
KhalilFarouqi <k.farouqi@itroadgroup.com>
2023-03-13 10:46:22 +0100

    add Dto

---
+++ Car_Rental/src/main/java/com/app/dto/UserDto.java
@@ -0,0 +1,20 @@
+package com.app.dto;
+
+import com.app.entity.enem.Roles;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.*;
+
+import java.util.Date;
+import java.util.List;
+
+@Getter
+@Setter
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public class UserDto {
+    private Long ID;
+    private Date Last_CheckIn;
+    private String UserName;
+    private String PassWord;
+    private Roles roles;
+    private List<OrderDto> orderDtoList;
+}

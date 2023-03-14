7aced6c3580565e506bbac252f57e9815da92574
KhalilFarouqi <k.farouqi@itroadgroup.com>
2023-03-14 17:23:34 +0100

    add Admin Repository

---
+++ Car_Rental/src/main/java/com/app/repository/AdminRepo.java
@@ -0,0 +1,9 @@
+package com.app.repository;
+
+import com.app.entity.Admin;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface AdminRepo extends BaseJpaRepository<Admin> {
+
+}

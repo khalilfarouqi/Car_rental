428012c00c0ab35ff7bc318f5cfc1ae32ce1e0b2
KhalilFarouqi <k.farouqi@itroadgroup.com>
2023-03-14 17:08:46 +0100

    extend in other interface

--- Car_Rental/src/main/java/com/app/repository/NotificationGroupeRepo.java
+++ Car_Rental/src/main/java/com/app/repository/NotificationGroupeRepo.java
@@ -1,9 +1,8 @@
 package com.app.repository;
 
 import com.app.entity.NotificationGroupe;
-import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface NotificationGroupeRepo extends JpaRepository<NotificationGroupe, Long> {
+public interface NotificationGroupeRepo extends BaseJpaRepository<NotificationGroupe> {
 }

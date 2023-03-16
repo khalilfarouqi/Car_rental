bfde35f797b33eb92dea63dbb8731dc0984a616f
KhalilFarouqi <k.farouqi@itroadgroup.com>
2023-03-16 10:46:08 +0100

    Add bean in main application

--- Car_Rental/src/main/java/com/app/CarRentalApplication.java
+++ Car_Rental/src/main/java/com/app/CarRentalApplication.java
@@ -2,15 +2,23 @@ package com.app;
 
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.info.Info;
+import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @OpenAPIDefinition(info = @Info(title = "CAR RENTAL API", version = "2.0", description = "CAR RENTAL Information"))
+@EnableScheduling
 public class CarRentalApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CarRentalApplication.class, args);
+	}
+	@Bean
+	public ModelMapper modelMapper(){
+		return new ModelMapper();
 	}
 
 }

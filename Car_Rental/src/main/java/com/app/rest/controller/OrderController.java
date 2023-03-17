566251eed7b148ee084c14f637e32bcbf6881ecc
KhalilFarouqi <k.farouqi@itroadgroup.com>
2023-03-17 16:47:58 +0100

    change CRUD of Order

--- Car_Rental/src/main/java/com/app/rest/controller/OrderController.java
+++ Car_Rental/src/main/java/com/app/rest/controller/OrderController.java
@@ -1,13 +1,42 @@
 package com.app.rest.controller;
 
+import com.app.dto.OrderDto;
 import com.app.rest.api.OrderApi;
+import com.app.sevice.impl.ServiceOrderImpl;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @Slf4j
 @RequiredArgsConstructor
 @RestController
 public class OrderController implements OrderApi {
+    private final ServiceOrderImpl serviceOrder;
 
+    @Override
+    public List<OrderDto> getAllOrder() {
+        return serviceOrder.getAllOrder();
+    }
+
+    @Override
+    public OrderDto getOrderById(Long id) {
+        return serviceOrder.getOrderById(id);
+    }
+
+    @Override
+    public void saveOrder(OrderDto orderDto) {
+        serviceOrder.saveOrder(orderDto);
+    }
+
+    @Override
+    public void updateOrder(OrderDto orderDto) {
+        serviceOrder.updateOrder(orderDto);
+    }
+
+    @Override
+    public void deleteOrder(Long id) {
+        serviceOrder.deleteOrder(id);
+    }
 }
--- Car_Rental/src/main/java/com/app/sevice/impl/ServiceOrderImpl.java
+++ Car_Rental/src/main/java/com/app/sevice/impl/ServiceOrderImpl.java
@@ -5,40 +5,40 @@ import com.app.repository.OrderRepo;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.modelmapper.ModelMapper;
-import org.modelmapper.convention.MatchingStrategies;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import com.app.entity.Order;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.ArrayList;
 import java.util.List;
-import java.util.stream.Collectors;
 
 @Slf4j
 @Transactional(readOnly = true)
 @RequiredArgsConstructor
 @Service
 public class ServiceOrderImpl {
-    @Autowired
-    private OrderRepo orderRepository;
-    @Autowired
-    private ModelMapper modelMapper;
+    private final OrderRepo orderRepository;
+    private final ModelMapper modelMapper;
 
     public List<OrderDto> getAllOrder(){
-        return orderRepository.findAll()
-                .stream()
-                .map(this::convertEntityToDto)
-                .collect(Collectors.toList());
-    }
-    private OrderDto convertEntityToDto(Order order){
-        modelMapper.getConfiguration()
-                .setMatchingStrategy(MatchingStrategies.LOOSE);
-        return modelMapper.map(order, OrderDto.class);
+        List<OrderDto> orderDtoList = new ArrayList<>();
+        orderRepository.findAll().forEach(element -> orderDtoList.add(modelMapper.map(element, OrderDto.class)));
+        return orderDtoList;
     }
 
-    private Order convertDtoToEntity(OrderDto orderDto){
-        modelMapper.getConfiguration()
-                .setMatchingStrategy(MatchingStrategies.LOOSE);
-        return modelMapper.map(orderDto, Order.class);
+    public OrderDto getOrderById(Long id){
+        return modelMapper.map(orderRepository.findById(id), OrderDto.class);
+    }
+
+    public void saveOrder(OrderDto orderDto){
+        orderRepository.save(modelMapper.map(orderDto, Order.class));
+    }
+
+    public void updateOrder(OrderDto orderDto){
+        orderRepository.save(modelMapper.map(orderDto, Order.class));
+    }
+
+    public void deleteOrder(Long id){
+        orderRepository.deleteById(id);
     }
 }

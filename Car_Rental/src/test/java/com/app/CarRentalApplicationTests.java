93c582b538b6ab275d37b96a507d0997b96d74c9
KhalilFarouqi <k.farouqi@itroadgroup.com>
2023-03-17 14:51:22 +0100

    clean test

--- Car_Rental/src/test/java/com/app/CarRentalApplicationTests.java
+++ Car_Rental/src/test/java/com/app/CarRentalApplicationTests.java
@@ -1,60 +1,8 @@
 package com.app;
 
-import com.app.dto.CarDto;
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-
-import com.app.entity.Car;
-import com.app.entity.Client;
-import com.app.sevice.impl.ServiceCarImpl;
-import com.app.sevice.impl.ServiceClientImpl;
 
 @SpringBootTest
 class CarRentalApplicationTests {
-
-	@Test
-	void contextLoads() {
-		
-		/*Client client = new Client();
-		
-		client.setAdress("adress1");
-		client.setAge(25);
-		client.setCIN("J521847");
-		client.setCantry("Morocco");
-		client.setCity("Agadir");
-		client.setEmail("email@gmail.com");
-		client.setFirst_Name("khalil");
-		client.setLast_Name("farouqi");
-		client.setLicence("10/123456");
-		client.setTel("0638153545");
-		client.setVol_Numbre(1);
-		client.setUserName("0638153545");
-		client.setPassWord("0638153545");
-		
-		ServiceClientImpl adminController = new ServiceClientImpl();
-		
-		try {
-			adminController.save(client);
-		} catch (Exception e) {
-			System.out.println(e.getMessage());
-		}
-
-		adminController.save(client);*/
-		
-		CarDto car = new CarDto();
-		
-		car.setName("Login");
-		
-		ServiceCarImpl servCar = new ServiceCarImpl();
-		
-		try {
-			servCar.save(car);
-		} catch (Exception e) {
-			System.out.println(e.getMessage());
-		}
-		
-		servCar.save(car);
-		
-	}
 
 }

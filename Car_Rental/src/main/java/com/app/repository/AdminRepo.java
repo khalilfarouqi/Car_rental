63a8ec5499e34fec153e6c0f5534f3d3aa0bc158
KhalilFarouqi <k.farouqi@itroadgroup.com>
2023-03-17 16:18:30 +0100

    remove admin

--- Car_Rental/src/main/java/com/app/repository/AdminRepo.java
+++
@@ -1,9 +0,0 @@
-package com.app.repository;
-
-import com.app.entity.Admin;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface AdminRepo extends BaseJpaRepository<Admin> {
-
-}

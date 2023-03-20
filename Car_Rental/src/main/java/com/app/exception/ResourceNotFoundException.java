aeaff8bfa0aac0ac24e2e35b3ff24b4c43f118c9
KhalilFarouqi <k.farouqi@itroadgroup.com>
2023-03-20 14:20:09 +0000

    clean Not Fond Exception

--- Car_Rental/src/main/java/com/app/exception/ResourceNotFoundException.java
+++ Car_Rental/src/main/java/com/app/exception/ResourceNotFoundException.java
@@ -7,27 +7,14 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 public class ResourceNotFoundException extends RuntimeException {
 
 	private static final long serialVersionUID = 1L;
-	private String resourceName;
-	private String fieldName;
-	private Object fieldValue;
-	
-	public ResourceNotFoundException(String resourceName, Long id, String fieldName, Object fieldValue) {
-		super(String.format("%s not found with %s : '%s'", resourceName, fieldName, fieldValue));
+	private final String resourceName;
+
+	public ResourceNotFoundException(String resourceName) {
+		super(String.format("%s not found with %s : '%s'", resourceName));
 		this.resourceName = resourceName;
-		this.fieldName = fieldName;
-		this.fieldValue = fieldValue;
 	}
 
 	public String getResourceName() {
 		return resourceName;
 	}
-
-	public String getFieldName() {
-		return fieldName;
-	}
-
-	public Object getFieldValue() {
-		return fieldValue;
-	}
-
 }
